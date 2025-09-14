bc79fb1f9603bb99e0fa9e375c6ad975794b62a7
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 14:18:05 +0200

    Update KeyboardArrows.js

--- KeyboardArrows.js
+++ KeyboardArrows.js
@@ -1,4 +1,8 @@
 // KeyboardArrows.js — versione classica (no modules), espone window.KeyboardArrows
+/*
+© 2025 Alessandro Pezzali. Tutti i diritti riservati.
+Licenza d’Uso — Il Cubo di Rubik PWA
+*/
 (function () {
   'use strict';
 
