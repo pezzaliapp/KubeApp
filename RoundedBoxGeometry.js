a35b9e15d83b85dd90ef9a4734263275e22082f9
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 14:19:26 +0200

    Update RoundedBoxGeometry.js

--- RoundedBoxGeometry.js
+++ RoundedBoxGeometry.js
@@ -1,4 +1,9 @@
 // RoundedBoxGeometry.js — versione classica (no modules), espone window.RoundedBoxGeometry
+/*
+© 2025 Alessandro Pezzali. Tutti i diritti riservati.
+Licenza d’Uso — Il Cubo di Rubik PWA
+*/
+
 (function () {
   'use strict';
 
