ef152c01cbfe710720e050dcce2ceeeb8a96ce94
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 14:19:50 +0200

    Update RoundedPlaneGeometry.js

--- RoundedPlaneGeometry.js
+++ RoundedPlaneGeometry.js
@@ -1,4 +1,9 @@
 // RoundedPlaneGeometry.js — versione classica (no modules), espone window.RoundedPlaneGeometry
+/*
+© 2025 Alessandro Pezzali. Tutti i diritti riservati.
+Licenza d’Uso — Il Cubo di Rubik PWA
+*/
+
 (function () {
   'use strict';
 
