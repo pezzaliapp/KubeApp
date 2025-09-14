04a8b3a2cfbe32ac7938310809aa6140ef45656e
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 14:15:45 +0200

    Update Draggable.js

--- Draggable.js
+++ Draggable.js
@@ -1,4 +1,8 @@
 // Draggable.js — versione classica (no modules), espone window.Draggable
+/*
+© 2025 Alessandro Pezzali. Tutti i diritti riservati.
+Licenza d’Uso — Il Cubo di Rubik PWA
+*/
 (function () {
   const THREE_NS = window.THREE;
 
