195a2c9a9bbd6957843a24ed104ae14991733a8c
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 14:13:11 +0200

    Update Controls.js

--- Controls.js
+++ Controls.js
@@ -1,4 +1,8 @@
 // Controls.js (ES module)
+/*
+© 2025 Alessandro Pezzali. Tutti i diritti riservati.
+Licenza d’Uso — Il Cubo di Rubik PWA
+*/
 import { Tween, Easing } from './Tween.js';
 import { Draggable } from './Draggable.js';
 
