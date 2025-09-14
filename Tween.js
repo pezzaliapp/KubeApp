084d37afaef46622ddfef600397919e5eabb96e9
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 13:27:53 +0200

    Update Tween.js

--- Tween.js
+++ Tween.js
@@ -1,103 +1,91 @@
-import { Animation } from './Animation.js';
-import { Easing } from './Easing.js';
+// Tween.js — versione classica (no modules), dipende da window.Animation e window.Easing
+(function () {
+  'use strict';
 
-class Tween extends Animation {
+  class Tween extends window.Animation {
+    constructor(options) {
+      super(false);
 
-  constructor( options ) {
+      options = options || {};
 
-    super( false );
+      this.duration   = options.duration || 500;
+      this.easing     = options.easing || (t => t);
+      this.onUpdate   = options.onUpdate || (() => {});
+      this.onComplete = options.onComplete || (() => {});
 
-    this.duration = options.duration || 500;
-    this.easing = options.easing || ( t => t );
-    this.onUpdate = options.onUpdate || ( () => {} );
-    this.onComplete = options.onComplete || ( () => {} );
+      this.delay = options.delay || false;
+      // Se yoyo è truthy, abilita ping-pong: parte avanti (yoyo=false), poi inverte
+      this.yoyo = options.yoyo ? false : null;
 
-    this.delay = options.delay || false;
-    this.yoyo = options.yoyo ? false : null;
+      this.progress = 0;
+      this.value    = 0;
+      this.delta    = 0;
 
-    this.progress = 0;
-    this.value = 0;
-    this.delta = 0;
+      this.getFromTo(options);
 
-    this.getFromTo( options );
+      if (this.delay) setTimeout(() => super.start(), this.delay);
+      else super.start();
 
-    if ( this.delay ) setTimeout( () => super.start(), this.delay );
-    else super.start();
-
-    this.onUpdate( this );
-
-  }
-
-  update( delta ) {
-
-    const old = this.value * 1;
-    const direction = ( this.yoyo === true ) ? - 1 : 1;
-
-    this.progress += ( delta / this.duration ) * direction;
-
-    this.value = this.easing( this.progress );
-    this.delta = this.value - old;
-
-    if ( this.values !== null ) this.updateFromTo();
-
-    if ( this.yoyo !== null ) this.updateYoyo();
-    else if ( this.progress <= 1 ) this.onUpdate( this );
-    else {
-
-      this.progress = 1;
-      this.value = 1;
-      this.onUpdate( this );
-      this.onComplete( this );
-      super.stop();      
-
+      // primo aggiornamento "a freddo" per applicare gli stati iniziali
+      this.onUpdate(this);
     }
 
-  }
+    update(delta) {
+      const old = this.value * 1;
+      const direction = (this.yoyo === true) ? -1 : 1;
 
-  updateYoyo() {
+      this.progress += (delta / this.duration) * direction;
 
-    if ( this.progress > 1 || this.progress < 0 ) {
+      this.value = this.easing(this.progress);
+      this.delta = this.value - old;
 
-      this.value = this.progress = ( this.progress > 1 ) ? 1 : 0;
-      this.yoyo = ! this.yoyo;
+      if (this.values !== null) this.updateFromTo();
 
+      if (this.yoyo !== null) {
+        this.updateYoyo();
+      } else if (this.progress <= 1) {
+        this.onUpdate(this);
+      } else {
+        this.progress = 1;
+        this.value = 1;
+        this.onUpdate(this);
+        this.onComplete(this);
+        super.stop();
+      }
     }
 
-    this.onUpdate( this )
-
-  }
-
-  updateFromTo() {
-
-    this.values.forEach( key => {
-
-      this.target[ key ] = this.from[ key ] + ( this.to[ key ] - this.from[ key ] ) * this.value;
-
-    } );
-
-  }
-
-  getFromTo( options ) {
-
-    if ( ! options.target || ! options.to ) {
-
-      this.values = null;
-      return;
-
+    updateYoyo() {
+      if (this.progress > 1 || this.progress < 0) {
+        this.value = this.progress = (this.progress > 1) ? 1 : 0;
+        this.yoyo = !this.yoyo; // inverti direzione
+      }
+      this.onUpdate(this);
     }
 
-    this.target = options.target || null;
-    this.from = options.from || {};
-    this.to = options.to || null;
-    this.values = [];
+    updateFromTo() {
+      this.values.forEach(key => {
+        this.target[key] = this.from[key] + (this.to[key] - this.from[key]) * this.value;
+      });
+    }
 
-    if ( Object.keys( this.from ).length < 1 )
-      Object.keys( this.to ).forEach( key => { this.from[ key ] = this.target[ key ]; } );
+    getFromTo(options) {
+      if (!options.target || !options.to) {
+        this.values = null;
+        return;
+      }
 
-    Object.keys( this.to ).forEach( key => { this.values.push( key ) } );
+      this.target = options.target || null;
+      this.from   = options.from || {};
+      this.to     = options.to   || null;
+      this.values = [];
 
+      if (Object.keys(this.from).length < 1) {
+        Object.keys(this.to).forEach(key => { this.from[key] = this.target[key]; });
+      }
+      Object.keys(this.to).forEach(key => { this.values.push(key); });
+    }
   }
 
-}
-
-export { Tween, Easing };
+  window.Tween = Tween;
+  // window.Easing è definito nel file Easing.js separato
+})();
