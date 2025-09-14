81097594840c64d8297075189e509a81616fb729
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-14 14:23:24 +0200

    Update Transition.js

--- Transition.js
+++ Transition.js
@@ -1,4 +1,10 @@
 // Transition.js — versione classica (no modules), usa window.Tween/Easing ed espone window.Transition
+
+/*
+© 2025 Alessandro Pezzali. Tutti i diritti riservati.
+Licenza d’Uso — Il Cubo di Rubik PWA
+*/
+
 (function () {
   'use strict';
 
