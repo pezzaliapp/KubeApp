15a0c458a31a44ccf9679f9ef1209fb3a3b48136
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-20 13:31:07 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "kubeapp-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install: pre-caching delle risorse
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate: pulizia cache vecchie
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: risponde prima dalla cache, poi dalla rete
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
